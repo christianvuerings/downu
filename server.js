cbc0893ee8a7260db230ea6d4d92fd7e3a678997
Christian Vuerings <vueringschristian@gmail.com>
2015-03-02 10:49:00 -0800

    Use https only

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ var path = require('path');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
+var env = process.env.NODE_ENV || 'development';
 
-app.use('/', express.static(__dirname + '/public'));
+/**
+ * Since we're showing personal pictures, make sure we only pass data over https
+ */
+var forceSSL = function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+/**
+ * Configuration
+ */
+app.configure(function () {
+
+  // When in production, force SSL
+  if (env === 'production') {
+    app.use(forceSSL);
+  }
+
+  // List public for the static assets
+  app.use('/', express.static(__dirname + '/public'));
+});
 
 var port = process.env.PORT || 5000;
 server.listen(port);
